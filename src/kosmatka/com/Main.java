89584dd6941e8e3684d5a439f8d8a0dc924d9a20
Adam <kosmatkassh@gmail.com>
2021-11-03 13:51:39 +0100

    1st commit

---
+++ src/kosmatka/com/Main.java
@@ -0,0 +1,14 @@
+package kosmatka.com;
+
+public class Main {
+    public static void main(String[] args){
+        int x = 0;
+
+
+        System.out.println("New project test");
+
+        System.out.println("test 2");
+    }
+
+
+}
